a510fc22072cae8023c31e5531ee482c80452685
Nur Sayid Tamam <nstamam70@gmail.com>
2026-06-09 21:36:41 +0700

    Merge branch 'features' from origin

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -4,10 +4,52 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.User;
+
 /**
  *
  * @author ryumaaa
  */
 public class UserDAO {
+     private Connection conn;
+ 
+    public UserDAO() {
+        this.conn = Connection.getConnection();
+    }
     
+    // kode untuk mengecek username & password
+    
+     public User login(String username, String password) {
+        User user = null;
+        String sql = "SELECT * FROM user WHERE username = ? AND password = MD5(?) AND status = 'aktif'";
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                user = mapResultSet(rs);
+            }
+        } catch (SQLException e) {
+            System.err.println("Error login: " + e.getMessage());
+        }
+        return user;
+    }
+     
+     public List<User> getAll() {
+        List<User> list = new ArrayList<>();
+        String sql = "SELECT * FROM user ORDER BY id_user ASC";
+        try (Statement st = conn.createStatement();
+             ResultSet rs = st.executeQuery(sql)) {
+            while (rs.next()) {
+                list.add(mapResultSet(rs));
+            }
+        } catch (SQLException e) {
+            System.err.println("Error getAll user: " + e.getMessage());
+        }
+        return list;
+    }
 }
--- src/model/Kriteria.java
+++ src/model/Kriteria.java
@@ -4,10 +4,109 @@
  */
 package model;
 
+import java.sql.Date;
+
 /**
  *
  * @author ryumaaa
  */
 public class Kriteria {
-    
+
+    private int idKriteria;
+    private String kodeKriteria;
+    private String namaKriteria;
+    private String deskripsi;
+    private double bobotAkhir;
+    private String status;
+    private Date createdAt;
+
+    public Kriteria() {
+    }
+
+    public Kriteria(int idKriteria, String kodeKriteria, String namaKriteria,
+            String deskripsi, double bobotAkhir,
+            String status, Date createdAt) {
+        this.idKriteria = idKriteria;
+        this.kodeKriteria = kodeKriteria;
+        this.namaKriteria = namaKriteria;
+        this.deskripsi = deskripsi;
+        this.bobotAkhir = bobotAkhir;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
+
+    public Kriteria(String kodeKriteria, String namaKriteria,
+            String deskripsi, String status) {
+        this.kodeKriteria = kodeKriteria;
+        this.namaKriteria = namaKriteria;
+        this.deskripsi = deskripsi;
+        this.status = status;
+    }
+
+    public int getIdKriteria() {
+        return idKriteria;
+    }
+
+    public void setIdKriteria(int idKriteria) {
+        this.idKriteria = idKriteria;
+    }
+
+    public String getKodeKriteria() {
+        return kodeKriteria;
+    }
+
+    public void setKodeKriteria(String kodeKriteria) {
+        this.kodeKriteria = kodeKriteria;
+    }
+
+    public String getNamaKriteria() {
+        return namaKriteria;
+    }
+
+    public void setNamaKriteria(String namaKriteria) {
+        this.namaKriteria = namaKriteria;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public double getBobotAkhir() {
+        return bobotAkhir;
+    }
+
+    public void setBobotAkhir(double bobotAkhir) {
+        this.bobotAkhir = bobotAkhir;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Kriteria{"
+                + "idKriteria=" + idKriteria
+                + ", kodeKriteria='" + kodeKriteria + '\''
+                + ", namaKriteria='" + namaKriteria + '\''
+                + ", bobotAkhir=" + bobotAkhir
+                + ", status='" + status + '\''
+                + '}';
+    }
 }
